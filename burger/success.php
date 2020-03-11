1444901e565027eb09a796d1ce364ea559de3dbd
Alex <iamdrakoice@gmail.com>
2020-03-11 22:28:21 +0200

    ДЗ №3 правки 11.03.2020

--- burger/success.php
+++ burger/success.php
@@ -19,37 +19,35 @@ $comment = htmlspecialchars($_POST['comment']) ?? null;
 $call = htmlspecialchars($_POST['call']) ?? null;
 $payment = htmlspecialchars($_POST['payment']) ?? null;
 
-
 $patime = date('H-i');
 $pathname = $patime . '.txt';
 
 $query = $db->prepare("select user.email, user.id from user where user.email ='$email'");
 $query->execute();
-$userId = $db->lastInsertId();
+$userId = $query->fetch(PDO::FETCH_ASSOC)['id'];
 
-if ($userId == 0) {
+if ($userId == 0 || $userId == null) {
     $userSend = $db->query("insert into burger.user(email, name, phone) VALUES ('$email','$name','$phone')");
-    $userId = $db->lastInsertId();
-    $orderSend = $db->query("insert into burger.orders(home, street, structure, flat, floor, comment, payment, bell, user_id)
-    values ('$home', '$street', '$structure', '$flat', '$floor', '$comment', '$payment', '$call','$userId')");
-    $querys = $db->prepare("select count(*) from user where email ='$email'");
-    $querys->execute();
-    $count = $querys->fetch(PDO::FETCH_ASSOC);
-    if ($count["count(*)"] > 1) {
-        $message =
-            "Заказ №" . $userId . "
+}
+
+$userIdOrder = $userId;
+$orderSend = $db->query("insert into burger.orders(home, street, structure, flat, floor, comment, payment, bell, user_id)
+    values ('$home', '$street', '$structure', '$flat', '$floor', '$comment', '$payment', '$call','$userIdOrder')");
+$querys = $db->prepare("select count(*) from user where email ='$email'");
+$querys->execute();
+$count = $querys->fetch(PDO::FETCH_ASSOC);
+
+if ($count["count(*)"] > 1) {
+    $message =
+        "Заказ №" . $userIdOrder . "
             Ваш заказ будет доставлен по адресу улица " . $street . ", дом " . $home . "
             Заказ DarkBeefBurger за 500 рублей, 1 шт
             Спасибо - это ваш " . $count["count(*)"] . " заказ";
-    } else {
-        $message =
-            'Заказ №' . $userId . '
+} else {
+    $message =
+        'Заказ №' . $userIdOrder . '
             Ваш заказ будет доставлен по адресу улица ' . $street . ', дом ' . $home . '
             Заказ DarkBeefBurger за 500 рублей, 1 шт
             Спасибо - это ваш первый заказ';
-    }
-    file_put_contents("mail/$pathname", $message);
-    echo "one";
-} else {
-    echo "was not written in to BD";
 }
+file_put_contents("mail/$pathname", $message);
